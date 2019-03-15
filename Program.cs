e39b49e16d436e99535e6eb8449bf3429f2b3c4d
Dylan Attal <attal.dylan@gmail.com>
2019-03-15 14:18:49 -0400

    Basic game with 7 hardcoded as correct number.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,50 @@
 
 namespace csharp_number_guesser
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      // Ask user name
+      Console.WriteLine("What is your name?");
+      // Get user input
+      string userName = Console.ReadLine();
+
+      // Start game
+      Console.WriteLine($"Hello, {userName}, let's play the number guessing game!");
+
+      // Set the correct number
+      int correctNumber = 7;
+      // Set guess
+      int guess = 0;
+
+      // Ask user to guess a number
+      Console.WriteLine("Guess a number between 1 and 10!");
+      // Play rounds of game
+      while (guess != correctNumber)
+      {
+        string userGuess = Console.ReadLine();
+        // Change guess to int and store as guess
+        guess = Int32.Parse(userGuess);
+        // Match guess to correctNumber
+        if (guess != correctNumber)
         {
-            Console.WriteLine("Hello World!");
+          // Change text color
+          Console.ForegroundColor = ConsoleColor.Red;
+          // Wrong guess message
+          Console.WriteLine("Wrong number! Guess again!");
+          // Reset text color
+          Console.ResetColor();
         }
+      }
+      // Change text color
+      Console.ForegroundColor = ConsoleColor.Green;
+      // Correct guess message
+      Console.WriteLine("Correct number! Good job!");
+      // Reset text color
+      Console.ResetColor();
+
+
     }
+  }
 }
